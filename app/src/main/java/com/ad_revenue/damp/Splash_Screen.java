d52572d176fd458800ba604a451dbc7a50419cb9
Aaron Andrews <superryuk@gmail.com>
2017-04-13 20:44:12 -0400

    Test commit:  *Added a yeah boiiii

--- app/src/main/java/com/ad_revenue/damp/Splash_Screen.java
+++ app/src/main/java/com/ad_revenue/damp/Splash_Screen.java
@@ -9,5 +9,6 @@ public class Splash_Screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash__screen);
+        //yeah boiiii
     }
 }
