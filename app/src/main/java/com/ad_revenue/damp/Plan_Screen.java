6ae3856ebf6b3467a506dc93eb20d506419cb504
Aaron Andrews <superryuk@gmail.com>
2017-04-17 18:04:59 -0400

    Added Plan Screen *Added an individual plan screen with a scrollabe list of steps and some buttons that match our prototype   (They lead to nothing right now though.) *Made entries in the view plans screen clickable, the first entry leads to the individual plan screen.
    ***There's some contstraint layout complaints, I'll let Adrian handle that.

---
+++ app/src/main/java/com/ad_revenue/damp/Plan_Screen.java
@@ -0,0 +1,56 @@
+package com.ad_revenue.damp;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class Plan_Screen extends AppCompatActivity {
+
+    String[] steps = {"Do this.", "Then this.", "And that too", "Do this.", "Then this.", "And that too", "Then this.", "And that too"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plan__screen);
+
+        CustomAdapter adapter = new CustomAdapter(this,steps);
+        ListView listView = (ListView) findViewById(R.id.stepList);
+        listView.setAdapter(adapter);
+    }
+
+    class CustomAdapter extends ArrayAdapter<String>
+    {
+        Context context;
+        String[] title;
+
+
+        CustomAdapter(Context c, String[] title)
+        {
+
+            super(c, R.layout.single_step,title);
+            this.context = c;
+            this.title=title;
+
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            LayoutInflater vi = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View row = vi.inflate(R.layout.single_step, parent, false);
+            TextView titlee = (TextView) row.findViewById(R.id.stepText);
+            int pos = position+1;
+            titlee.setText(+pos + ". " + title[position]);
+            pos++;
+            return row;
+        }
+
+    }
+}
--- app/src/main/java/com/ad_revenue/damp/View_Plans.java
+++ app/src/main/java/com/ad_revenue/damp/View_Plans.java
@@ -1,14 +1,16 @@
 package com.ad_revenue.damp;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class View_Plans extends AppCompatActivity {
+public class View_Plans extends ListActivity {
 
     String[] stuff = {"Rick's Renal Dialysis Plan"};
 
@@ -16,15 +18,18 @@ public class View_Plans extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view__plans);
-        ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.activity_singleplan,stuff);
-        //System.out.println("yah boy");
-        ListView listView = (ListView) findViewById(R.id.viewPlansList);
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.activity_singeplan,stuff);
+        ListView listView = this.getListView();
         listView.setAdapter(adapter);
 
+    }
 
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        // TODO Auto-generated method stub
+        super.onListItemClick(l, v, position, id);
 
-
-
-
+        Intent intent = new Intent(this, Plan_Screen.class);
+        startActivity(intent);
     }
 }
